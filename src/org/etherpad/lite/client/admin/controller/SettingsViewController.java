2f33046543691a8aec2f566e870800cb7ee24736
Gared <mu.stefan@googlemail.com>
2013-02-26 15:02:44 +0100

    Fix #5: Add validation of URI

--- src/org/etherpad/lite/client/admin/controller/SettingsViewController.java
+++ src/org/etherpad/lite/client/admin/controller/SettingsViewController.java
@@ -1,7 +1,17 @@
 package org.etherpad.lite.client.admin.controller;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.etherpad.lite.client.admin.model.EPLite;
 import org.etherpad.lite.client.admin.view.MainView;
@@ -22,7 +32,11 @@ public class SettingsViewController {
 	}
 
 	private void initData() {
-		settingsView.setHostTextFieldText(epLite.getUrl());
+		String url = epLite.getUrl();
+		settingsView.setSslCheckBoxSelected(url.startsWith("https://"));
+		String hostText = url.split("//")[1];
+		
+		settingsView.setHostTextFieldText(hostText);
 		settingsView.setApiTextFieldText(epLite.getApiKey());
 	}
 
@@ -30,7 +44,7 @@ public class SettingsViewController {
 		settingsView.setSaveButtonListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				epLite.setServer(settingsView.getHostTextFieldText(), settingsView.getApiTextFieldText());
+				epLite.setServer(settingsView.getProtocolLabelText() + settingsView.getHostTextFieldText(), settingsView.getApiTextFieldText());
 				mainView.enableAdministrationMenu();
 				settingsView.dispose();
 			}
@@ -39,6 +53,30 @@ public class SettingsViewController {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				settingsView.dispose();
+			}
+		});
+		
+		settingsView.setHostTextFieldFocusListener(new FocusAdapter() {
+			public void focusLost(FocusEvent e) {
+		        JTextField textField = (JTextField) e.getSource();
+		        if (textField.getText().matches("^[a-zA-Z0-9-_\\.]*(:(6553[0-5]|655[0-2][0-9]|65[0-4][0-9][0-9]|6[0-4][0-9][0-9][0-9]|\\d{2,4}|[1-9]))?$")) {
+		        	textField.setBorder(null);
+		        } else {
+		        	Border border = BorderFactory.createLineBorder(Color.red);
+		        	textField.setBorder(border);
+		        }
+		    }
+		});
+		
+		settingsView.setSslCheckBoxChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent arg0) {
+				JCheckBox checkBox = (JCheckBox) arg0.getSource();
+				if (checkBox.isSelected()) {
+					settingsView.setProtocolLabelText("https://");
+				} else {
+					settingsView.setProtocolLabelText("http://");
+				}
 			}
 		});
 	}
--- src/org/etherpad/lite/client/admin/view/SettingsView.java
+++ src/org/etherpad/lite/client/admin/view/SettingsView.java
@@ -5,18 +5,23 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.event.ChangeListener;
 
 public class SettingsView extends JDialog {
 	private JButton saveButton = null;
 	private JButton cancelButton = null;
 	private JTextField hostTextField = null;
 	private JTextField apiTextField = null;
+	private JLabel protocolLabel = null;
+	private JCheckBox sslCheckBox = null;
 
 	public SettingsView() {
 		super(null, JDialog.DEFAULT_MODALITY_TYPE);
@@ -45,34 +50,50 @@ public class SettingsView extends JDialog {
 		mainPanel.add(hostLabel, c);
 		JLabel apiLabel = new JLabel("API Key: ");
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = 2;
 		c.weightx = 0;
 		mainPanel.add(apiLabel, c);
 
-		hostTextField = new JTextField(20);
-		hostTextField.setToolTipText("URL: http://example.com:9001");
+		protocolLabel = new JLabel("http://");
 		c.gridx = 1;
 		c.gridy = 0;
-		c.weightx = 1;
+		c.weightx = 0.2;
+		mainPanel.add(protocolLabel, c);
+		
+		hostTextField = new JTextField(20);
+		hostTextField.setToolTipText("URL: example.com:9001");
+		c.gridx = 2;
+		c.gridy = 0;
+		c.weightx = 0.8;
 		mainPanel.add(hostTextField, c);
-
-		apiTextField = new JTextField(20);
-		apiTextField.setToolTipText("APIKEY: See APIKEY.txt on server");
+		
+		sslCheckBox = new JCheckBox("SSL");
 		c.gridx = 1;
 		c.gridy = 1;
 		c.weightx = 1;
+		c.gridwidth = 2;
+		mainPanel.add(sslCheckBox, c);
+		
+		apiTextField = new JTextField(20);
+		apiTextField.setToolTipText("APIKEY: See APIKEY.txt on server");
+		c.gridx = 1;
+		c.gridy = 2;
+		c.weightx = 1;
+		c.gridwidth = 2;
 		mainPanel.add(apiTextField, c);
 
 		saveButton = new JButton("Save");
 		c.gridx = 0;
-		c.gridy = 2;
+		c.gridy = 3;
 		c.weightx = 0;
+		c.gridwidth = 1;
 		mainPanel.add(saveButton, c);
 
 		cancelButton = new JButton("Cancel");
 		c.gridx = 1;
-		c.gridy = 2;
+		c.gridy = 3;
 		c.weightx = 0;
+		c.gridwidth = 2;
 		mainPanel.add(cancelButton, c);
 
 		getContentPane().add(mainPanel);
@@ -84,6 +105,10 @@ public class SettingsView extends JDialog {
 
 	public void setHostTextFieldText(String hostTextFieldText) {
 		this.hostTextField.setText(hostTextFieldText);
+	}
+	
+	public void setHostTextFieldFocusListener(FocusAdapter a) {
+		this.hostTextField.addFocusListener(a);
 	}
 
 	public String getApiTextFieldText() {
@@ -100,5 +125,26 @@ public class SettingsView extends JDialog {
 
 	public void setCancelButtonListener(ActionListener actionListener) {
 		cancelButton.addActionListener(actionListener);
+	}
+	
+	public void setSslCheckBoxChangeListener(ChangeListener c) {
+		sslCheckBox.addChangeListener(c);
+	}
+	
+	public void setSslCheckBoxSelected(boolean selected) {
+		sslCheckBox.setSelected(selected);
+		if (selected) {			
+			protocolLabel.setText("https://");
+		} else {
+			protocolLabel.setText("http://");
+		}
+	}
+	
+	public void setProtocolLabelText(String text) {
+		protocolLabel.setText(text);
+	}
+	
+	public String getProtocolLabelText() {
+		return protocolLabel.getText();
 	}
 }
